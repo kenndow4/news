cba6a2d4818904a548d5fefa4a45a8cbe4f09812
kenedy <kendowcent@gmail.com>
2023-11-19 15:24:20 -0400

    agrege avatar

--- news/src/js/publicacion.js
+++ news/src/js/publicacion.js
@@ -1,8 +1,19 @@
+
+
+
+
 const contenedor = document.getElementById("cont-padre-public");
 const Publicacion=async()=>{
 
     // esta es la url de la api
     const  url = 'https://www.alphavantage.co/query?function=NEWS_SENTIMENT&tickers=AAPL&apikey=demo';
+    const ht= 'https://randomuser.me/api/';
+     const stado =await fetch(ht);
+     const dat = await stado.json();
+     const  avatar = dat.results[0];
+ 
+     
+    
     // aqui estoy acediendo al estado del input
 
     try{
@@ -12,18 +23,18 @@ const Publicacion=async()=>{
         // aqui lo estoy transformando a json
         const datos = await resultado.json();
         const result = datos.feed;
-        console.log(result);
+        console.log(result[0]);
 
         result.forEach(re => {
             
           const x = document.createElement("div");
           x.className="contenedorPublicacion";
           x.innerHTML=`
-          
+          <img  src="${avatar.picture.large}" alt="Foto de Publicacion" width="100px">
+          <p class="autor">Autor: <i>${re.authors[0]}</i></p>
           <div class="imgCont">
           <img class="imagensita" src="${re.banner_image}" alt="Foto de Publicacion" width="700px">
       </div>
-      <p class="autor">Autor: <i>${re.authors[0]}</i></p>
       <a href="${re.url}" target="_blank"><p class="link">Mas informacion</p></a>
       <p class="descripcion">${re.title}</p>
           
