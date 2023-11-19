5def6e668eb5666ff531237a29f35b8721b96cca
kenedy <kendowcent@gmail.com>
2023-11-19 12:37:00 -0400

    Obtuve la api

--- news/public/bundle.js
+++ news/public/bundle.js
@@ -1,2 +1,17 @@
 'use strict';
 
+const Publicacion=async()=>{
+
+    // esta es la url de la api
+    const  url = 'https://www.alphavantage.co/query?function=NEWS_SENTIMENT&tickers=AAPL&apikey=demo';
+    // aqui estoy acediendo al estado del input
+
+    const resultado = await fetch(url);
+
+    // aqui lo estoy transformando a json
+    const datos = await resultado.json();
+
+    console.log(datos);
+};
+
+Publicacion();
--- news/src/js/publicacion.js
+++ news/src/js/publicacion.js
@@ -1,1 +1,16 @@
 
+const Publicacion=async()=>{
+
+    // esta es la url de la api
+    const  url = 'https://www.alphavantage.co/query?function=NEWS_SENTIMENT&tickers=AAPL&apikey=demo';
+    // aqui estoy acediendo al estado del input
+
+    const resultado = await fetch(url);
+
+    // aqui lo estoy transformando a json
+    const datos = await resultado.json();
+
+    console.log(datos);
+}
+
+export default Publicacion ;
